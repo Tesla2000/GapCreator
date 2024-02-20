3997da30bc15d6b55e750749788a57776f0059ae
Tesla2000 <fratajczak124@gmail.com>
2024-02-20 14:56:44 +0100

    Added a random choice of an option

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from .Config import Config
 from .ml import get_longest_masks
 from .conv_masked_sentence_to_gap import conv_masked_sentence_to_gap
@@ -9,10 +11,10 @@ def main():
         "When one group of people dominates a field of study, whether it's an age group, a cultural group or a gender, there is danger of creating a narrow view of the subject. Including more women in male-dominated areas will bring in fresh points of view, new talent and creativity. It can also help increase women's social and financial position in some countries. ")
     for sentence in sentences:
         if len(sentence.split()) > Config.maks_sentence_length:
-            print("Skipping: ", sentence)
+            print(sentence, end='. ')
             continue
-        for masked_sentence in get_longest_masks(sentence):
-            print(conv_masked_sentence_to_gap(masked_sentence))
+        masked_sentence = random.choice(get_longest_masks(sentence))
+        print(conv_masked_sentence_to_gap(masked_sentence), end='. ')
 
 
 if __name__ == '__main__':
