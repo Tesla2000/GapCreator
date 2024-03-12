166c81b79efcacd8ed1d66cf4df034f779d8f3f2
Tesla2000 <fratajczak124@gmail.com>
2024-03-12 14:27:05 +0100

    Made replacements in website contents possible

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 class Config:
     possible_options = 1
     max_sentence_length = 127
-    confidence_threshold = .95
+    confidence_threshold = .9
     max_gap_length = 3
     mask = "[MASK]"
--- main.py
+++ main.py
@@ -10,19 +10,29 @@ from string_functions.div_to_sentences import div_to_sentences
 
 def main():
     text_file_path = Path('input.txt')
-    sentences = div_to_sentences(text_file_path.read_text())
-    for raw_sentence in sentences:
-        raw_sentence = re.sub(r'\s+', ' ', raw_sentence)
-        sentence = raw_sentence.lower().replace(',', ' ').strip('.?!')
-        if len(sentence.split()) > Config.max_sentence_length:
-            print(sentence)
-            continue
-        options = get_longest_masks(sentence)
-        if not options:
-            print(sentence)
-            continue
-        masked_sentence = random.choice(options)
-        print(conv_masked_sentence_to_gap(raw_sentence, masked_sentence))
+    output_file = Path('output.txt').open('w')
+    start_index = 0
+    for paragraph in re.finditer(r'<p>([^<]+)</p>', text_file_path.read_text()):
+        output_file.write(paragraph.string[start_index:paragraph.regs[1][0]])
+        output_file.write('<p>')
+        start_index = paragraph.regs[1][1]
+        sentences = div_to_sentences(paragraph.string[paragraph.regs[1][0]:start_index])
+        for raw_sentence in sentences:
+            raw_sentence = re.sub(r'\s+', ' ', raw_sentence)
+            write_with_last_sign = lambda sentence: output_file.write(sentence + (raw_sentence[-1] if not sentence.endswith(raw_sentence[-1]) else '') + ' ')
+            sentence = raw_sentence.lower().replace(',', ' ').strip('.?!')
+            if len(sentence.split()) > Config.max_sentence_length:
+                write_with_last_sign(sentence)
+                continue
+            options = get_longest_masks(sentence)
+            if not options:
+                write_with_last_sign(sentence)
+                continue
+            masked_sentence = random.choice(options)
+            write_with_last_sign(conv_masked_sentence_to_gap(raw_sentence, masked_sentence))
+        output_file.write('</p>')
+    output_file.write(paragraph.string[start_index:])
+    output_file.close()
 
 
 if __name__ == '__main__':
