cf4c45236f43bd5495c511c1a14d30903d3aec79
Tesla2000 <fratajczak124@gmail.com>
2024-03-20 21:45:19 +0100

    Updated to more reasonable version

--- ml.py
+++ ml.py
@@ -1,5 +1,3 @@
-from itertools import count
-
 import torch
 import torch.nn.functional as F
 from transformers import BertTokenizer, BertForMaskedLM
@@ -18,7 +16,9 @@ batched_attention_masks = [[1, 1] * 64] * batch_size
 tokens_tensor = torch.tensor(batched_indexed_tokens)
 segments_tensor = torch.tensor(batched_segment_ids)
 attention_masks_tensor = torch.tensor(batched_attention_masks)
-mlm_model_ts = BertForMaskedLM.from_pretrained('bert-base-uncased', torchscript=True)
+model_name = 'bert-base-uncased'
+mlm_model_ts = BertForMaskedLM.from_pretrained(model_name, torchscript=True)
+tokenizer = BertTokenizer.from_pretrained(model_name)
 traced_mlm_model = torch.jit.trace(mlm_model_ts, [tokens_tensor, segments_tensor, attention_masks_tensor])
 mlm_model_ts.eval()
 stripped_signs = '.?!,'
@@ -38,10 +38,27 @@ def get_longest_masks(base_sentence: str) -> tuple[str, ...]:
         original_sentences = tuple(masked_sentences)
         token_probabilities = []
         for sentence_index in range(len(masked_sentences)):
-            for position in pos_masks[sentence_index]:
-                sentence = masked_sentences[sentence_index]
+            sentence = masked_sentences[sentence_index]
+            tokenized_masked_positions = []
+            tokenized_counter = 0
+            word_to_tokens = {}
+            for word_index in range(len(sentence)):
+                word = sentence[word_index]
+                if word not in word_to_tokens:
+                    word_to_tokens[word] = tokenizer.tokenize(word)
+                if word != Config.mask:
+                    tokenized_counter += len(word_to_tokens[word])
+                else:
+                    word = base_sentence.split()[word_index]
+                    if word not in word_to_tokens:
+                        word_to_tokens[word] = tokenizer.tokenize(word)
+                    for _ in word_to_tokens[word]:
+                        tokenized_counter += 1
+                        tokenized_masked_positions.append(tokenized_counter)
+            pass
+            for position in tokenized_masked_positions:
                 encoded_inputs = enc(
-                    sentence,
+                    ' '.join(sentence),
                     return_tensors='pt', padding='max_length', max_length=Config.max_sentence_length + 1)
                 output = mlm_model_ts(**encoded_inputs)[0][0][position]
                 next_token_probability = torch.sum(torch.sort(F.softmax(output, dim=0))[0][-Config.possible_options:])
